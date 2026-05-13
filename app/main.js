b68e87ee6b8ff05e51229fa82368dab743cf5bea
Ben Horley-Owen <benhorleyowen@outlook.com>
2026-05-13 10:34:33 +0100

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,19 +1,43 @@
+const { type } = require("os");
+const { exit } = require("process");
 const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   prompt: "$ ",
+  builtin: {
+    echo: (command) => {
+      args = command.slice(5).split(" ");
+      console.log(args.join(" "));
+    },
+    type: (command) => {
+      instruction = command.split(" ")[0];
+      args = command.slice(5).split(" ");
+      if (args.length > 1) { // error check
+        console.log("type: too many arguments");
+      }
+      if (args.length === 0) { // error check
+        console.log("type: too few arguments");
+      }
+      if (instruction in self.builtin) { // if the command is in builtin, return builtin
+        console.log(`${instruction} is a shell builtin`);
+      } 
+      else {
+        console.log(`${instruction}: not found`);
+      }
+    },
+    exit: () => {
+      rl.close();
+    }
+  }
 });
 
 rl.on('line', (command) => {
-  if (command === "exit") {
-    rl.close();
-    return;
-  }
-  else if (command.startsWith("echo ")) {
-    console.log(`${command.slice(5)}`); //slices "echo " from the command
-  } else {
+  instruction = command.split(" ")[0];
+  if (instruction in rl.builtin) { //call shell builtins
+    rl.builtin[instruction](command);
+  } else { // unrecognised command
     console.log(`${command}: command not found`);
   }
   rl.prompt();
